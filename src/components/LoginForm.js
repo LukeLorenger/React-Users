45203691a2a80bd0d1ba89fc5a6704b3dd7504e8
Luke Lorenger <dirtydeadtv@gmail.com>
2021-07-15 12:12:39 -0400

    all details are passing through within login button

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -16,15 +16,15 @@ function LoginForm({ Login, error }) {
                 {/* ERROR! */}
                 <div className="form-group">
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" id="name"/>
+                    <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email: </label>
-                    <input type="email" name="email" id="email"/>
+                    <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password: </label>
-                    <input type="password" name="password" id="password"/>
+                    <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password} />
                 </div>
                 <input type="submit" value="LOGIN"/>
             </div>
